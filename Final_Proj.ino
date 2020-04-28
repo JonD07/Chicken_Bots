962ef04ae06cd563fb386a14f7502d229e430fa9
jond07 <jonathan.a.diller@gmail.com>
2020-04-27 21:54:01 -0400

    Added obsticle avoidence

--- Final_Proj.ino
+++ Final_Proj.ino
@@ -4,6 +4,9 @@
 
 //
 /// Global constants
+const int SENSOR_TEST = 0;
+const int SPEED = 75;
+const int LED = 4;
 
 // Distance sensors
 const int echoPin1 = 2;		// Right sensor
@@ -12,16 +15,16 @@ const int echoPin2 = 12;	// Left sensor
 const int trigPin2 = 13;
 
 // Motor Driver
-const int m1LogicA = 8;		// Left Wheel
-const int m1LogicB = 9;
+const int m1LogicA = 5;		// Left Wheel
+const int m1LogicB = 6;
 const int m2LogicA = 10;	// Right Wheel
 const int m2LogicB = 11;
 
 // IR Sensors
 // '1' => line detected
 // '0' => no line detected
-const int leftIRPin = 7;
-const int rightIRPin = 6;
+const int leftIRPin = 8;
+const int rightIRPin = 7;
 
 const int sensorPin = A0;
 
@@ -47,12 +50,14 @@ int lightCal;
 int lightVal;
 
 int state;
+int test;
 
 float distance1, distance2;
 
 void setup() {
 	//
 	/// Set pin-modes
+	pinMode(LED, OUTPUT);
 	// Dist sensors
 	pinMode(trigPin1, OUTPUT);
 	pinMode(echoPin1, INPUT);
@@ -70,7 +75,7 @@ void setup() {
 	//
 	/// Initial config
 	state = A;
-	
+	test = 0;
 	// Set motor driver pins LOW
 	digitalWrite(m1LogicA, LOW);
 	digitalWrite(m1LogicB, LOW);
@@ -84,29 +89,37 @@ void setup() {
 }
 
 void loop() {
-	// Gather Data
-	/*
-	distance1 = getDistance(trigPin1, echoPin1);
-	distance2 = getDistance(trigPin2, echoPin2);
-	left_sensor = digitalRead(leftIRPin);
-	right_sensor = digitalRead(rightIRPin);
+	if(SENSOR_TEST) {
+		test++;
+		if(test & 0x064) {
+			digitalWrite(LED, HIGH);
+		}
+		else {
+			digitalWrite(LED, LOW);
+		}
+		// Gather Data
+		distance2 = getDistanceL();
+		distance1 = getDistanceR();
+		left_sensor = digitalRead(leftIRPin);
+		right_sensor = digitalRead(rightIRPin);
 
-	lightVal = analogRead(sensorPin);
-*/
-/*
-	Serial.print("Dist R: ");
-	Serial.print(distance1);
-	Serial.print("\tDist L: ");
-	Serial.print(distance2);
-//	Serial.print("\tPhoto: ");
-//	Serial.println(lightVal);
-	Serial.print("\tL: ");
-	Serial.print(left_sensor);
-	Serial.print("\tR: ");
-	Serial.println(right_sensor);
-*/
+		lightVal = analogRead(sensorPin);
 
-	state = runStateMachine();
+
+		Serial.print("Dist R: ");
+		Serial.print(distance1);
+		Serial.print("\tDist L: ");
+		Serial.print(distance2);
+	//	Serial.print("\tPhoto: ");
+	//	Serial.println(lightVal);
+		Serial.print("\tL: ");
+		Serial.print(left_sensor);
+		Serial.print("\tR: ");
+		Serial.println(right_sensor);
+	}
+	else {
+		state = runStateMachine();
+	}
 }
 
 int runStateMachine() {
@@ -122,9 +135,16 @@ int runStateMachine() {
 				return H;
 			}
 			
-			/*
-			TODO: add B & C trans here
-			*/
+			// Check left distance sensor
+			if(getDistanceL() < 10.0) {
+				Right();
+				return A;
+			} 
+			// Check right distance sensor
+			if(getDistanceR() < 10.0) {
+				Left();
+				return A;
+			}
 			
 			/*
 			TODO: add D trans here
@@ -147,7 +167,7 @@ int runStateMachine() {
 			}
 			
 			Brake();
-			//TODO: turn on LED
+			digitalWrite(LED, HIGH);
 			return K;
 		}
 		
@@ -164,7 +184,7 @@ int runStateMachine() {
 			}
 			
 			Brake();
-			//TODO: turn on LED
+			digitalWrite(LED, HIGH);
 			return K;
 		}
 		
@@ -211,13 +231,20 @@ int runStateMachine() {
 				Right_right();
 				return K;
 			}
-			
-			/*
-			TODO: add sensor 1 & 2 stuff here
-			*/
+		
+			// Check left distance sensor
+			if(getDistanceL() < 10.0) {
+				Brake();
+				return K;
+			}
+			// Check right distance sensor
+			if(getDistanceR() < 10.0) {
+				Brake();
+				return K;
+			}
 			
 			forward();
-			// TODO: LED
+			digitalWrite(LED, HIGH);
 			return K;
 		}
 		
@@ -227,9 +254,9 @@ int runStateMachine() {
 // forward
 void forward() {
 	digitalWrite(m1LogicA, LOW);
-	digitalWrite(m1LogicB, HIGH);
+	analogWrite(m1LogicB, SPEED);
 	digitalWrite(m2LogicA, LOW);
-	digitalWrite(m2LogicB, HIGH);
+	analogWrite(m2LogicB, SPEED);
 }
 
 // Brake
@@ -244,13 +271,13 @@ void Brake() {
 void Right() {
 	digitalWrite(m1LogicA, HIGH);
 	digitalWrite(m1LogicB, HIGH);
-	digitalWrite(m2LogicA, HIGH);
+	analogWrite(m2LogicA, SPEED);
 	digitalWrite(m2LogicB, LOW);
 }
 
 // Left(), turn left by reversing
 void Left() {
-	digitalWrite(m1LogicA, HIGH);
+	analogWrite(m1LogicA, SPEED);
 	digitalWrite(m1LogicB, LOW);
 	digitalWrite(m2LogicA, HIGH);
 	digitalWrite(m2LogicB, HIGH);
@@ -259,7 +286,7 @@ void Left() {
 // right(), turn right by moving forward
 void right() {
 	digitalWrite(m1LogicA, LOW);
-	digitalWrite(m1LogicB, HIGH);
+	analogWrite(m1LogicB, SPEED);
 	digitalWrite(m2LogicA, HIGH);
 	digitalWrite(m2LogicB, HIGH);
 }
@@ -269,34 +296,47 @@ void left() {
 	digitalWrite(m1LogicA, HIGH);
 	digitalWrite(m1LogicB, HIGH);
 	digitalWrite(m2LogicA, LOW);
-	digitalWrite(m2LogicB, HIGH);
+	analogWrite(m2LogicB, SPEED);
 }
 
 // Left_left(), turn left by spinning
 void Left_left() {
-	digitalWrite(m1LogicA, HIGH);
+	analogWrite(m1LogicA, SPEED);
 	digitalWrite(m1LogicB, LOW);
 	digitalWrite(m2LogicA, LOW);
-	digitalWrite(m2LogicB, HIGH);
+	analogWrite(m2LogicB, SPEED);
 }
 
 // Right_right(), turn right by spinning
 void Right_right() {
 	digitalWrite(m1LogicA, LOW);
-	digitalWrite(m1LogicB, HIGH);
-	digitalWrite(m2LogicA, HIGH);
+	analogWrite(m1LogicB, SPEED);
+	analogWrite(m2LogicA, SPEED);
 	digitalWrite(m2LogicB, LOW);
 }
 
-float getDistance(int trigPin, int echoPin) {
+float getDistanceR() {
 	float dist;
 	
-	digitalWrite(trigPin, LOW);
+	digitalWrite(trigPin1, LOW);
 	delayMicroseconds(2);
-	digitalWrite(trigPin, HIGH);
+	digitalWrite(trigPin1, HIGH);
 	delayMicroseconds(10);
-	digitalWrite(trigPin, LOW);
-	dist = pulseIn(echoPin, HIGH);
+	digitalWrite(trigPin1, LOW);
+	dist = pulseIn(echoPin1, HIGH);
+	
+	return (dist*0.0343)/2.0;
+}
+
+float getDistanceL() {
+	float dist;
+	
+	digitalWrite(trigPin2, LOW);
+	delayMicroseconds(2);
+	digitalWrite(trigPin2, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin2, LOW);
+	dist = pulseIn(echoPin2, HIGH);
 	
 	return (dist*0.0343)/2.0;
 }
